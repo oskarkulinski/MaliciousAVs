36c93953d91bdaa32a9106ce97bd3e41a5ad95a9
Oskar Kulinski <oskarkulinski3@gmail.com>
2025-06-23 23:41:38 +0200

    Added new version of plot script

--- plots_ok.py
+++ plots_ok.py
@@ -2,41 +2,51 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-tests = ["tabql_500", "tabql_700", "tabql_1034", "tabql_500_adapt", "tabql_700_adapt", "tabql_1034_adapt"]
+baselines = ["baseline_500_20", "baseline_500", "baseline_500_60"]
+tests = ["tabql3_500_20", "tabql3_500", "tabql3_500_60"]
+
+labels = ["500 cars/20% AVs", "500 cars/40% AVs", "500 cars/60% AVs"]
 
 col_pre = "t_HDV_pre"
 col_test = "t_HDV_test" 
 
 total_data = []
 
-for test in tests[3:]:
-    path = f"results/{test}/metrics/BenchmarkMetrics.csv"
+for test in tests:
+    path = f"results_common/{test}/metrics/BenchmarkMetrics.csv"
     df = pd.read_csv(path)
-    df["test"] = test
-
-
+    df["name"] = test
     total_data.append(df)
 
 combined_df = pd.concat(total_data, ignore_index=True)
+combined_df = combined_df.sort_values(by=['name']).reset_index(drop=True)
 
-combined_df = combined_df.sort_values(by=['test']).reset_index(drop=True)
+baseline_data = []
+for baseline in baselines:
+    path = f"results_common/{baseline}/metrics/BenchmarkMetrics.csv"
+    df = pd.read_csv(path)
+    df["name"] = baseline
+    baseline_data.append(df)
 
-values1 = combined_df[col_pre].tolist()
-values2 = combined_df[col_test].tolist()
+baseline_df = pd.concat(baseline_data, ignore_index=True)
+baseline_df = baseline_df.sort_values(by=["name"]).reset_index(drop=True)
+
+values1 = baseline_df[col_pre].tolist()
+values2 = combined_df[col_pre].tolist()
 
 fig, ax = plt.subplots(figsize=(16, 9)) 
 
-x = np.arange(len(tests[3:])) 
+x = np.arange(len(tests)) 
 width = 0.35  
 
-rects1 = ax.bar(x - width/2, values1, width, label=col_pre, color='skyblue')
-rects2 = ax.bar(x + width/2, values2, width, label=col_test, color='lightcoral')
+rects1 = ax.bar(x - width/2, values1, width, label="random AVs", color='skyblue')
+rects2 = ax.bar(x + width/2, values2, width, label="malicious AVs", color='lightcoral')
 
-ax.set_xlabel(f'Comparison of {col_pre} and {col_test} across all tests')
+ax.set_xlabel(f'Comparison of human travel time after adding malicious AVs vs random AVs')
 ax.set_ylabel('Seconds')
 ax.set_title(f'Human travel time before and after AVs are added to the network ')
 ax.set_xticks(x)
-ax.set_xticklabels(tests[3:], rotation=0, ha='right') 
+ax.set_xticklabels(labels, rotation=0, ha='right') 
 
 ax.set_ylim(bottom=0, top=350)
 
