ac5acef0876842c23c78dbb1f20a3d04f71e89a2
Oskar Kulinski <oskarkulinski3@gmail.com>
2025-06-24 17:51:29 +0200

    Added plots script for baseline

--- plots_ok.py
+++ plots_ok.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+import seaborn as sns
+from scipy.stats import pearsonr
+
 baselines = ["baseline_500_20", "baseline_500", "baseline_500_60"]
 tests = ["tabql3_500_20", "tabql3_500", "tabql3_500_60"]
 
@@ -31,8 +34,8 @@ for baseline in baselines:
 baseline_df = pd.concat(baseline_data, ignore_index=True)
 baseline_df = baseline_df.sort_values(by=["name"]).reset_index(drop=True)
 
-values1 = baseline_df[col_pre].tolist()
-values2 = combined_df[col_pre].tolist()
+values1 = baseline_df[col_test].tolist()
+values2 = combined_df[col_test].tolist()
 
 fig, ax = plt.subplots(figsize=(16, 9)) 
 
@@ -55,4 +58,29 @@ ax.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.tight_layout() 
 plt.show() 
+plt.show()
+
+av_ratios = [0.2, 0.4, 0.6]
+
+
+delta = np.array(values2) - np.array(values1)
+
+df = pd.DataFrame({
+    'AV_ratio': av_ratios,
+    'Time_Increase': delta
+})
+
+# Calculate correlation
+r, p = pearsonr(df['AV_ratio'], df['Time_Increase'])
+
+# Plot
+plt.figure(figsize=(10, 6))
+sns.regplot(x='AV_ratio', y='Time_Increase', data=df,
+            color='mediumseagreen', scatter_kws={'s': 100}, line_kws={'color': 'darkgreen'})
+plt.title(f"Correlation Between AV Ratio and Travel Time Increase after intruducing random AVs vs Malicious AVs:\n"
+          f"r = {r:.2f}, p = {p:.3g}")
+plt.xlabel("AV Ratio")
+plt.ylabel("Travel Time Increase (Random AVs - Malicious AVs, in seconds)")
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.tight_layout()
 plt.show()
